01d137e579c09054105a1a4796d9ad5be802cebc
Grayson Stiegler <682@holbertonschool.com>
2019-04-06 01:31:38 +0000

    just a test script

---
+++ test_shell_script.c
@@ -0,0 +1,32 @@
+#include <string.h>
+#include <stdlib.h>
+
+
+int main (void)
+{
+	string command;
+cout <<
+
+		while (command != "exit")
+
+		{
+			if (command == "ls")
+				system("ls");
+
+			else if (command == "cd")
+				system("cd");
+			else if (command == "mkdir")
+				system("mkdir");
+			else if (command == "date")
+				system("date");
+			else if (command == "help")
+				cout <<"ls\tdate\theko\texit\n";
+			else if (command != '\0')
+				count <<"ERROR\n";
+
+			cout <<"#shell--->>";
+			cin >> command;
+		}
+
+return (0);
+}
