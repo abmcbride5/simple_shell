009abe82d6f87cebd8e8cfd8c0fe4b767ef706f1
Grayson Stiegler <682@holbertonschool.com>
2019-04-08 19:41:25 +0000

    new header file for simple shell

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL
+#define SHELL
+
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(__attribute__((unused)) int argc, char *argv[]);
+int _strcmp(char *str1, char *str2);
