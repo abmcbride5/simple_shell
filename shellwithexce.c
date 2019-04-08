185b73d25bf9d0b13c1af29d567dce6de011a20c
Amber <abmcbride5@gmail.com>
2019-04-08 22:20:49 +0000

    adding shell with exce function working

---
+++ shellwithexce.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+int main(void)
+{
+	char *buf;
+	char *e = "exit\n";
+	char *gg[] = {"", NULL};
+	size_t l;
+	ssize_t c;
+	pid_t pid;
+	int status;
+
+
+
+
+	l = 0;
+	c = 0;
+	while (1)
+	{
+		buf = NULL;
+		write(STDOUT_FILENO, "$ ", 2);
+		c = getline(&buf, &l, stdin);
+		if (strcmp(buf, e) ==0)
+		{
+			exit (1);
+		}
+		buf[c-1] = '\0';
+		if (c > 1)
+		{
+			pid = fork();
+			if (pid == 0)
+			{
+				write(STDOUT_FILENO, buf, c);
+				execve(buf, gg, NULL);
+			}
+			if (pid > 0)
+			{
+				  if (strcmp(buf, e) == 0)
+				  {
+				  exit (1);
+				  }
+				wait(&status); 
+					
+			}
+
+		}
+	}
+	free(buf);
+	return(c);
+}
