6309ee6b3a8ea640128f9aec0769b97d85040827
Amber <abmcbride5@gmail.com>
2019-04-15 22:14:40 +0000

    this file is the basic loop that will run the shell. I basically just broke down the big program we have to comply with the function length

---
+++ shell.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "shell.h"
+/**
+*main- program that creates a prompt, recieves input and calls
+*forking function when needed.
+*
+*Return: void
+*/
+int main(void)
+{
+	char *buf = NULL;
+	char *e = "exit\n";
+	size_t l = 0;
+	ssize_t c = 0;
+
+	while (1)
+	{
+		write(STDOUT_FILENO, "$ ", 2);
+		c = getline(&buf, &l, stdin);
+		if (c == -1)
+		{
+			exit(1);
+		}
+		if (_strcmp(buf, e) == 0)
+		{
+			exit(1);
+		}
+		buf[c - 1] = '\0';
+		if (c > 1)
+		{
+			_forkIt(buf);
+		}
+	}
+	return (0);
+}
