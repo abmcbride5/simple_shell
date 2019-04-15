e58ca4ba0e1812ff9fa0ffa363951cdbaba91027
Amber <abmcbride5@gmail.com>
2019-04-15 23:06:37 +0000

    Updating header file with new protoype for forkIt function

--- shell.h
+++ shell.h
@@ -1,6 +1,5 @@
 #ifndef SHELL_H
 #define SHELL_H
-
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -16,4 +15,7 @@ int _putchar(char c);
 char **_parseline(char *buf, char *delim);
 int _strcmp(char *str1, char *str2);
 int _putchar(char c);
+char *do_exit(char *str);
+int findpath(void);
+void _forkIt(char *str);
 #endif
