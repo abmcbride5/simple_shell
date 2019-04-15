a38c22ff6df7e837bcddd5faab70f08f6402e6e1
Amber <abmcbride5@gmail.com>
2019-04-15 23:10:55 +0000

    updated helper function to include forkIt definition

--- helperfunc.c
+++ helperfunc.c
@@ -50,3 +50,37 @@ char **_parseline(char *buf, char *delim)
 	args[i] = NULL;
 	return (args);
 }
+/**
+*_forkIt- function to create a child process and execute commands
+*
+*@str: string that contains command
+*
+*Return: void
+*/
+void _forkIt(char *str)
+{
+	char **gg;
+	char *e = "exit\n";
+	pid_t pid;
+	int status = 0, er = 0;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		gg = _parseline(str, " ");
+		er = execve(gg[0], gg, NULL);
+		if (er == -1)
+		{
+			perror("./shell: No such file or directory found");
+			exit(1);
+		}
+	}
+	if (pid > 0)
+	{
+		if (_strcmp(str, e) == 0)
+		{
+			exit(1);
+		}
+		wait(&status);
+	}
+}
