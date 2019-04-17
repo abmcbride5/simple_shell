ba927fa09becb9e7b77aa9f38937fd3e925e7e29
Grayson Stiegler <682@holbertonschool.com>
2019-04-17 19:32:31 +0000

    no betty checks on _strcat.c anymore

--- _strcat.c
+++ _strcat.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 #include <string.h>
-#include <shell.h>
+#include "shell.h"
 
-char *_strcat(char* destination, const char* source, size_t num)
+/**
+ * _strcat - cat the string
+ *
+ * @destination: input for _strcat
+ *
+ * @source: input for the source
+ *
+ * @num: input for num
+ *
+ * Return: destination
+ */
+
+char *_strcat(char *destination, const char *source, size_t num)
 {
-	char* ptr = destination + strlen(destination);
+	char  *ptr = destination + strlen(destination);
 
 	while (*source != '\0' && num--)
 		*ptr++ = *source++;
